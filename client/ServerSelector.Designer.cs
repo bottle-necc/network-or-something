7654536fd3ec1b31348cf4ed8e939dbd430cdf76
bottle-necc <bottlenecc22@gmail.com>
2024-05-20 15:31:31 +0200

    Because of a critical error i am fully rewriting everything

---
+++ client/ServerSelector.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Client
+{
+    partial class ServerSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login = new System.Windows.Forms.Button();
+            this.tbxAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(53, 51);
+            this.Login.Margin = new System.Windows.Forms.Padding(2);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(56, 19);
+            this.Login.TabIndex = 12;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // tbxAddress
+            // 
+            this.tbxAddress.Location = new System.Drawing.Point(80, 16);
+            this.tbxAddress.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxAddress.Name = "tbxAddress";
+            this.tbxAddress.Size = new System.Drawing.Size(150, 20);
+            this.tbxAddress.TabIndex = 11;
+            this.tbxAddress.Text = "127.0.0.1";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(15, 19);
+            this.lblAddress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(61, 13);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "IP-Address:";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(137, 51);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(56, 19);
+            this.btnRegister.TabIndex = 13;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 81);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.tbxAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "ServerSelector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.TextBox tbxAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
+
